f5b095b203fe3a0022d3028bae28cfa096564644
dongdongmemeda <1390916847@qq.com>
2017-11-20 14:16:53 +0800

    update echarts component

--- component/chart/Geo.vue
+++ component/chart/Geo.vue
@@ -138,8 +138,6 @@ export default {
 </script>
 <style lang="less" scoped>
 .geo{
-    width: 100%;
-    height: 100%;
     .main{
         width: 100%;
         height: 100%;
--- component/chart/Pie.vue
+++ component/chart/Pie.vue
@@ -95,8 +95,6 @@ export default {
 </script>
 <style lang="less" scoped>
 .pie{
-    width: 100%;
-    height: 100%;
     .main{
         width: 100%;
         height: 100%;
